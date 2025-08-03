4503e3bb09e364ec30d2fec8e9829153e145be08
Aditya <anupama12sg@gmail.com>
2025-08-03 20:16:43 +0530

    chore: component Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hero from './components/Hero';
 import Navigation from './components/Navigation';
 
 const App = () => {
     return (
         <div>
             <Navigation />
+            <Hero/>
         </div>
     )
 }
