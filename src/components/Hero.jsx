df08bd52541dc91b4e0d7f90aafef6f656b04179
Aditya <anupama12sg@gmail.com>
2025-08-03 20:26:50 +0530

    chore: created 2 more divs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 const Hero = () => {
     return <main className="hero">
+
         <div className="hero-content">
             <h1>YOUR FEET DESERVE THE BEST</h1>
-            <p>Experience the ultimate comfort and style with our premium footwear collection.</p>
+            <p>Experience the ultimate comfort and style with our premium footwear collection. The perfect blend of quality and design awaits you.</p>
+        </div>
+
+        <div className="hero-btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+        </div>
+
+        <div className="shopping">
+            <p>Also available on</p>
         </div>
 
         <div className="hero-image"></div>
