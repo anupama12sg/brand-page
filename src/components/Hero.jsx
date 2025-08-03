c7e5963ed00bf8c82980b720dc47d0606051aa96
Aditya <anupama12sg@gmail.com>
2025-08-03 20:22:12 +0530

    chore: create 2 divs & added h1 and p into the first div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
 const Hero = () => {
-    return;
+    return <main className="hero">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>Experience the ultimate comfort and style with our premium footwear collection.</p>
+        </div>
 
+        <div className="hero-image"></div>
+    </main>
 }
 
 export default Hero
